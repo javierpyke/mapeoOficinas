d9a01fa26630d33bd9f5786749e768a11bf9d5fa
Javier Pyke <pyke.jn@gmail.com>
2023-05-15 14:02:39 -0300

    creacion test

---
+++ test/equipo.spec.js
@@ -0,0 +1,15 @@
+const expect = require('chai').expect
+
+const Equipo = require('../src/clases/equipo.js')
+
+console.log(Equipo)
+
+describe('Equipo', function(){
+    describe('#getMarca', function(){
+        it('GetMarca', function(){
+
+            expect(true)
+
+        })
+    })
+})
