e11ccd6696dc94b44450e71e4eef6f046371a0b5
Javier Pyke <pyke.jn@gmail.com>
2023-05-15 15:41:57 -0300

    creacion tipos de equipos

---
+++ src/tipos/tiposDeEquipos.js
@@ -0,0 +1,11 @@
+const TiposDeEquipos = {
+	Camara: "CAMARA",
+	Cpu: "CPU",
+	Teclado: "TECLADO",
+	LectorDeHuella: "LECTOR DE HUELLA",
+    PadDeFirma: "PAD DE FIRMA",
+    Monitor: "MONITOR",
+    Mouse: "MOUSE"
+}
+
+module.exports = TiposDeEquipos;
