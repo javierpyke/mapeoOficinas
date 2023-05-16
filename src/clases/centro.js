cb00c4800fd4fca22b5ac6a02679ec19b50b9f44
Javier Pyke <pyke.jn@gmail.com>
2023-05-16 13:15:42 -0300

    Modificacion clase centro

--- src/clases/centro.js
+++ src/clases/centro.js
@@ -1,11 +1,23 @@
+const PuestoDeConsulta = require("./puesto_de_consulta");
+
 class Centro{
     constructor(direccion,numeroDeCentro){
         this.direccion = direccion
         this.numeroDeCentro = numeroDeCentro
+        this.puestos = []
     }
 
     setEncargado(encargado){
-        this.encargado = encargado
+        if(!this.encargado){
+            this.encargado = encargado
+        }
+    }
+
+    eliminarEncargado(){
+        var encargado = this.encargado
+        this.encargado = null
+
+        return encargado
     }
 
     getEncargado(){
@@ -15,6 +27,37 @@ class Centro{
     getNumeroDeCentro(){
         return this.numeroDeCentro
     }
+
+    agregarPuesto(){
+        this.puestos.push(new PuestoDeConsulta())
+    }
+
+    eliminarPuesto(numeroDePuesto){      
+        var puestoEliminado = this.puestos.splice(numeroDePuesto-1,1)
+        return puestoEliminado
+        
+    }
+
+    habilitarPuesto(numeroDePuesto){
+        this.puestos[numeroDePuesto-1].habilitar()
+    }
+
+    agregarTeclado(teclado,numeroDePuesto){
+        this.puestos[numeroDePuesto-1].setTeclado(teclado)
+    }
+
+    agregarMouse(mouse,numeroDePuesto){
+        this.puestos[numeroDePuesto-1].setMouse(mouse)
+    }
+
+    agregarMonitor(monitor,numeroDePuesto){
+        this.puestos[numeroDePuesto-1].setMonitor(monitor)
+    }
+
+    agregarCpu(cpu,numeroDePuesto){
+        this.puestos[numeroDePuesto-1].setCpu(cpu)
+    }
+
 }
 
 module.exports = Centro;
