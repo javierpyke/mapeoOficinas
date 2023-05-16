f53772c94eea4c25a65814f100559f6127e0b05a
Javier Pyke <pyke.jn@gmail.com>
2023-05-16 14:47:30 -0300

    Modificacion de la clase Proveedor

--- src/clases/proveedor.js
+++ src/clases/proveedor.js
@@ -5,6 +5,42 @@ class Proveedor{
         this.telefonoSoporte = telefonoSoporte
     }
 
+    setRazonSocial(razonSocial){
+        if(!this.razonSocial){
+            this.razonSocial = razonSocial
+        }
+    }
+
+    eliminarRazonSocial(){
+        const razonSocialEliminada = this.razonSocial
+        this.razonSocial = null
+        return razonSocialEliminada
+    }
+
+    setCuit(cuit){
+        if(!this.cuit){
+            this.cuit = cuit
+        }
+    }
+
+    eliminarCuit(){
+        const cuitEliminado = this.cuit
+        this.cuit = null
+        return cuitEliminado
+    }
+
+    setTelefonoSoporte(telefonoSoporte){
+        if(!this.telefonoSoporte){
+            this.telefonoSoporte = telefonoSoporte
+        }
+    }
+
+    eliminarTelefonoSoporte(){
+        const telefonoEliminado = this.telefonoSoporte
+        this.telefonoSoporte = null
+        return telefonoEliminado
+    }
+
     getRazonSocial(){
         return this.razonSocial
     }
