da1627918fce17499ebe95bdf94878ea122daae1
Javier Pyke <pyke.jn@gmail.com>
2023-05-17 15:48:55 -0300

    Modificacion clase CpuFactory

--- src/clases/cpuFactory.js
+++ src/clases/cpuFactory.js
@@ -1,7 +1,10 @@
+const TiposDeEquipos = require("../tipos/tiposDeEquipos");
 const EquipoInventariable = require("./equipo_inventariable");
 
 class CpuFactory{
     crear(marca,modelo,inventario){
-        return new EquipoInventariable(marca,modelo,'cpu',inventario)
+        return new EquipoInventariable(marca,modelo,TiposDeEquipos.Cpu,inventario)
     }
 }
+
+module.exports = CpuFactory
