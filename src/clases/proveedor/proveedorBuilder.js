e93e553cd911f76e365aa4abbce5fb7938393e95
Javier Pyke <pyke.jn@gmail.com>
2023-06-14 17:48:59 -0300

    creacion de builder de proveedor

---
+++ src/clases/proveedor/proveedorBuilder.js
@@ -0,0 +1,32 @@
+const Proveedor = require('./proveedor')
+
+class ProveedorBuilder{
+    constructor(){
+        this.razonSocial = null
+        this.cuit = null
+        this.telefonoSoporte = null
+        this.proveedor = null
+    }
+    
+    setRazonSocial(razonSocial){
+        this.razonSocial = razonSocial
+        return this
+    }
+
+    setCuit(cuit){
+        this.cuit = cuit
+        return this
+    }
+
+    setTelefonoSoporte(telefonoSoporte){
+        this.telefonoSoporte = telefonoSoporte
+        return this
+    }
+
+    build(){
+        this.proveedor = new Proveedor(this.razonSocial,this.cuit,this.telefonoSoporte)
+        return this.proveedor
+    }
+
+
+}
