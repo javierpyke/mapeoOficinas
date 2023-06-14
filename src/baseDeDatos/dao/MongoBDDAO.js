cefbb87d0c89aa9576f1d3bf73eaacc621f4118c
Javier Pyke <pyke.jn@gmail.com>
2023-06-14 17:46:44 -0300

    creacion base de datos

---
+++ src/baseDeDatos/dao/MongoBDDAO.js
@@ -0,0 +1,42 @@
+const { MongoClient } = require('mongodb')
+
+module.exports = class MongoDBDao {
+  constructor(collection) {
+
+        const url = 'mongodb+srv://javier:javier77@cluster0.samd2iu.mongodb.net/?retryWrites=true&w=majority'
+        this.client = new MongoClient(url)
+
+        // Database Name
+        this.dbName = 'mapeoDeOficinas';
+        this.collection = collection;
+      }
+    
+      async conectar() {
+        try {
+          await this.client.connect()
+          this.db = this.client.db(this.dbName)
+          this.collection2 = this.db.collection(this.collection)
+        } catch (e) {
+          console.log("No se pudo conectar a MongoDB")
+        }
+      }
+
+      async agregar(objeto){
+        await this.collection2.insertOne(objeto)
+      }
+
+      desconectar(){
+        this.client.close()
+      }
+    
+      async obtenerTodas() {
+        /*const findResult = {}
+        try{
+            findResult = await collection.find().toArray();
+        } catch(e){
+            console.log("no")
+        }*/
+        const findResult = await this.collection2.find().toArray();
+        return findResult;
+      }
+    }
